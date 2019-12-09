275779dc623c09a9a883db5b730c924aa0c6deea
紫荆秋雪 <zjqx1991@163.com>
2019-12-10 00:06:52 +0800

    日志有问题

--- gts-manage-education/src/main/java/com/gootschool/education/handler/GlobalExceptionHandler.java
+++ gts-manage-education/src/main/java/com/gootschool/education/handler/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package com.gootschool.education.handler;
 
 import com.gootschool.common.handler.RevanException;
 import com.gootschool.common.response.RevanResponse;
+import com.gootschool.common.utils.RevanExceptionUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -14,6 +17,7 @@ public class GlobalExceptionHandler {
     @ResponseBody
     public RevanResponse error(Exception e) {
         e.printStackTrace();
+        log.error(RevanExceptionUtil.getMessage(e));
         return RevanResponse.error().message("出现了异常");
     }
 
@@ -23,6 +27,7 @@ public class GlobalExceptionHandler {
     @ResponseBody
     public RevanResponse error(RevanException e) {
         e.printStackTrace();
+        log.error(RevanExceptionUtil.getMessage(e));
         return RevanResponse.error(e.getCodeEnum());
     }
 }
